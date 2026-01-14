a5fa4202f7ea30f33e3b8709ebd1fb298d671ab4
kttermgame <katherinexnz@gmail.com>
2026-01-14 10:54:45 +0700

    Create haydayGoods.js

---
+++ src/data/haydayGoods.js
@@ -0,0 +1,33 @@
+// src/data/haydayGoods.js
+export const HAYDAY_ITEMS = [
+  {
+    id: "hamburger",
+    name: "Hamburger",
+    nameTh: "แฮมเบอร์เกอร์",
+    level: 75,
+    category: "bakery",
+    inStock: true,
+    pricePer5: 1,
+    img: "https://static.wikia.nocookie.net/hayday/images/a/a4/Hamburger.png",
+  },
+  {
+    id: "honey",
+    name: "Honey",
+    nameTh: "น้ำผึ้ง",
+    level: 39,
+    category: "farm",
+    inStock: true,
+    pricePer5: 1,
+    img: "https://static.wikia.nocookie.net/hayday/images/8/8d/Honey.png",
+  },
+  {
+    id: "hot_dog",
+    name: "Hot Dog",
+    nameTh: "ฮอทดอก",
+    level: 75,
+    category: "bakery",
+    inStock: true,
+    pricePer5: 1,
+    img: "https://static.wikia.nocookie.net/hayday/images/0/0d/Hot_Dog.png",
+  },
+];
